e830941b4f0ad9a723eb61c1cd4aefbad45ff863
piyush0901 <piy0901@gmail.com>
2018-03-02 00:32:00 +0530

    Initial

---
+++ models/Tweet.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	"userid"            : {"type" : mongoose.Schema.Types.ObjectId, ref: 'Details'},
+	"text"			    : {"type" : String, "required" : true},
+	"RetweetCount"      : {"type" : Number, "required" : true}
+ 
+})
+
+mongoose.model("Details",userSchema,"details");
+
