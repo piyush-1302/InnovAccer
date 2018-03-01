6a96da1df0d3bc83e0c9f07ef9784d51c6d8531d
piyush0901 <piy0901@gmail.com>
2018-03-02 00:29:07 +0530

    hi

---
+++ Desktop/InnovAccer/models/Details.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	"name" 				: {"type" : String, "required" : true},
+	"screenName"		: {"type" : String, "required" : true},
+	"location" 			: {"type" : String, "required" : true},
+	"followersCount"    : {"type" : Number, "required" : true},
+	"friendsCount" 		: {"type" : Number, "required" : true},
+	"listedCount" 		: {"type" : Number, "required" : true},
+	"favouritesCount"   : {"type" : Number, "required" : true},
+	"language" 			: {"type" : String, "reuired" : true}
+ 
+})
+
+mongoose.model("Details",userSchema,"details");
+
--- Desktop/InnovAccer/server.js
+++ Desktop/InnovAccer/server.js
@@ -6,8 +6,14 @@ const twit = require('twitter');
 const request = require('request')
 const app = express();
 const port = process.env.PORT || 7000;
-mongoose.connect(process.env.MONGOLAB_URI||"mongodb://pk1302:kr1302@ds137246.mlab.com:37246/innovaccer");
-
+var config = require('./config');
+mongoose.connect(process.env.MONGOLAB_URI||"mongodb://pk1302:kr1302@ds137246.mlab.com:37246/innovaccer"	);
+require('./models/Details');
+require('./models/Tweet');
+require('./models/Retweet');
+var userDetails = mongoose.model('Details');
+var tweets = mongoose.model('Tweet');
+var retweetUder = mongoose.model('Retweet');
 
 
 app.listen(port,function(err){
@@ -16,3 +22,27 @@ app.listen(port,function(err){
 	else
 		console.log("Server is up at " + port);
 });
+
+
+
+
+
+/*
+
+
+40
+down vote
+accepted
+JavaScript has built-in support for dates. First, to get your string into a Date object:
+
+date =  new Date('Thu Dec 29 2011 20:14:56 GMT-0600 (CST)')
+Now you can use various methods on the date to get the data you need:
+
+date.toDateString() // "Thu Dec 29 2011"
+date.toUTCString()  // "Fri, 30 Dec 2011 02:14:56 GMT"
+date.getMonth()     // 11
+date.getDate()      // 29
+date.getFullYear()  // 2011
+
+
+*/
